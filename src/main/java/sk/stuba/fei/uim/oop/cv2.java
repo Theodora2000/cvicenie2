80e91b0ef57d09bb383f2a83c61ea4c2a3b1a869
Theodora2000 <xsimont@stuba.sk>
2021-02-27 17:58:20 +0100

    2.4-Bubble Sort

--- src/main/java/sk/stuba/fei/uim/oop/cv2.java
+++ src/main/java/sk/stuba/fei/uim/oop/cv2.java
@@ -3,24 +3,31 @@ import java.util.Scanner;
 
 public class cv2 {
     public static void main(String[] args){
-        int[] pole = new int[10];
+        int[] pole = new int[5];
         System.out.println("Zadavajte cisla:");
         for(int i=0;i<pole.length;i++){
             pole[i]= Zklavesnice.readInt();
         }
-        //System.out.println("Vypis pola:");
-        for (int i=0;i<pole.length-1;i++){
-            for (int j=i+1;j<pole.length;j++){
-                if(pole[i]>pole[j]){
+
+        boolean imalijos ;
+        int smanji= 1;
+        do{
+            imalijos = false;
+            for(int i=0;i<(pole.length-smanji);i++){
+                if(pole[i]>pole[i+1]){
                     int temp = pole[i];
-                    pole[i]=pole[j];
-                    pole[j]=temp;
+                    pole[i]=pole[i+1];
+                    pole[i+1]=temp;
+                    imalijos = true;
                 }
             }
+            smanji++;
+        }while(imalijos);
 
-        }
+
         for (int i=0;i<pole.length;i++){
             System.out.println(pole[i]);
         }
+
             }
 }

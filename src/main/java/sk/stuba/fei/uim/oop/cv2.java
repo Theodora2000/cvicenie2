2a911175a0594a7ad9418c2c022dd49f99638cac
Theodora2000 <xsimont@stuba.sk>
2021-03-06 14:16:40 +0100

    2.6

--- src/main/java/sk/stuba/fei/uim/oop/cv2.java
+++ src/main/java/sk/stuba/fei/uim/oop/cv2.java
@@ -1,24 +1,32 @@
 package sk.stuba.fei.uim.oop;
-import java.util.Arrays;
-
 
 public class cv2 {
     public static void main(String[] args){
 
-            int [][] pole =  new int[3][3];
-            for (int i=0;i<pole.length;i++){
-                //pole[i]=new int [i+1];
-                for (int j=0;j<pole[i].length;j++){
-                    pole[i][j]=Zklavesnice.readInt();
-                }
+        int [][] matr =  new int[3][3];
+        for (int i = 0; i< matr.length; i++){
+            for (int j = 0; j< matr[i].length; j++){
+                matr[i][j]=Zklavesnice.readInt();
             }
+        }
 
-            for(int i=0;i<pole.length;i++){
-                for (int j=0;j<pole[i].length;j++){
-                    System.out.print(pole[i][j]+ " ");
-                }
-                System.out.println();
+        for(int i = 0; i< matr.length; i++){
+            for (int j = 0; j< matr[i].length; j++){
+                System.out.print(matr[i][j]+ " ");
             }
+            System.out.println();
+        }
+        int x,y,z;
+        int vysledok=0;
+        x = (matr[1][1] * matr[2][2]) - (matr[2][1] * matr[1][2]);
+        y = (matr[1][0] * matr[2][2]) - (matr[2][0] * matr[1][2]);
+        z = (matr[1][0] * matr[2][1]) - (matr[2][0] * matr[1][1]);
+
+        vysledok = (matr[0][0] * x)- (matr[0][1] * y) + (matr[0][2] * z);
+
+        System.out.println("Vysledok: " +vysledok);
+
+
     }
 }
 
